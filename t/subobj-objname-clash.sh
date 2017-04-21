21e4494a10f4b95b88d2574aed711562d0e0e110
Thomas Martitz <kugel@rockbox.org>
2017-04-21 15:25:52 +0200

    WIP

--- t/subobj-objname-clash.sh
+++ t/subobj-objname-clash.sh
@@ -22,28 +22,48 @@ mkdir -p src
 
 cat >> configure.ac << 'END'
 AC_PROG_CC
+AC_PROG_RANLIB
 AC_OUTPUT
 END
 
 cat > Makefile.am << 'END'
 AUTOMAKE_OPTIONS = subdir-objects foreign
 noinst_PROGRAMS = foo
-foo_SOURCES = src/foo.c
+foo_SOURCES = src/foo.c src/main.c
 foo_CPPFLAGS = -DVAL=0
+noinst_LIBRARIES = libbar.a
+libbar_a_SOURCES = src/foo.c
+#noinst_PROGRAMS += bar
+#bar_SOURCES = src/main.c
+#bar_LDADD = libbar.a
 include src/local.mk
 END
 
 cat > src/local.mk << 'END'
 noinst_PROGRAMS += src/foo
 src_foo_CPPFLAGS = -DVAL=1
-src_foo_SOURCES = src/foo.c
+src_foo_SOURCES = src/foo.c src/main.c
+#noinst_PROGRAMS += src/bar
+#noinst_LIBRARIES += src/libbar.a
+#src_bar_SOURCES = src/main.c
+#src_bar_LDADD = src/libbar.a
 END
 
 cat > src/foo.c << 'END'
 int
-main ()
+foo ()
 {
   return VAL;
+}
+END
+
+cat > src/main.c << 'END'
+int foo (void);
+
+int
+main ()
+{
+  return foo ();
 }
 END
 
@@ -55,4 +75,6 @@ $AUTOMAKE --add-missing
 $MAKE
 ./foo || fail_ "./foo should return 0"
 ./src/foo && fail_ "./src/foo should return 1"
+#~ ./bar || fail_ "./foo should return 0"
+#~ ./src/bar && fail_ "./src/foo should return 1"
 $MAKE clean
